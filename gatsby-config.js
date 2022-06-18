b6769a3cba57a288aebcbe91ef16eae80d656119
Stefan Barkman <thebarkman@users.noreply.github.com>
2022-06-18 12:34:57 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Das Blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Stefan Barkman`,
+      summary: `Troubleshooting wizard.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `Explore. Create. Inspire.`,
+    siteUrl: `https://www.dasblog.xyz`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `stefan_barkman`,
     },
   },
   plugins: [
