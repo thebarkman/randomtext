5f4cabefbf74e8c684ccab502cb5383d2e81aba7
Stefan Barkman <thebarkman@users.noreply.github.com>
2022-06-19 09:55:35 +0200

    Update bio.js

--- src/components/bio.js
+++ src/components/bio.js
@@ -48,7 +48,7 @@ const Bio = () => {
           {` `}
           Follow on <a href={`https://twitter.com/${social?.twitter || ``}`}>
             Twitter
-          </a>| Hej
+          </a> | <a href="https://www.linkedin.com/in/stefanbarkman/">LinkedIn</a> |
         </p>
       )}
     </div>
